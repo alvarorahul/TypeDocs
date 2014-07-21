58ed302f5c4dbb3c0aa78074dccba4bc2b1ca154
Alvaro Dias <alvarorahul@hotmail.com>
2014-07-20 17:07:04 -0700

    removed app.ts and moved that chunk of code to index.html

--- TypeDocs.Web/app.ts
+++
@@ -1,9 +0,0 @@
-﻿/// <reference path="TypeScript/Program.ts" />
-module TypeDocs.Web {
-    window.onload = () => {
-        var element = <HTMLElement>document.getElementsByClassName("typedocs-web")[0],
-            program = new Program(element);
-
-        program.run("/Definitions/typedocs.d.ts");
-    };
-}
