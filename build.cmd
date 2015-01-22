663f89e647602c12e8de646372c336816496d335
Alvaro Dias <alvarorahul@hotmail.com>
2015-01-21 20:42:10 -0800

    BUGFIX: fix commandline build scripts

--- build.cmd
+++ build.cmd
@@ -1,6 +1,11 @@
 
 :INITIALIZE
 SET MSBUILDPATH="%WINDIR%\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe"
+IF EXIST "%PROGRAMFILES(x86)%\MsBuild\12.0" SET MSBUILDPATH="%PROGRAMFILES(x86)%\MsBuild\12.0\bin\MSBuild.exe"
+IF EXIST "%PROGRAMFILES(x86)%\MsBuild\14.0" SET MSBUILDPATH="%PROGRAMFILES(x86)%\MsBuild\14.0\bin\MSBuild.exe"
+
+ECHO %MSBUILDPATH%
+
 SET BINDIR="%~dp0bin"
 
 IF NOT EXIST %MSBuildPath% GOTO ERRORNOMSBUILD
@@ -11,9 +16,9 @@ IF NOT EXIST %BINDIR% MD %BINDIR%
 
 copy "%~dp0README.md" "%~dp0bin\README.md"
 
-%MSBUILDPATH% "%~dp0TypeDocs.sln"
+%MSBUILDPATH% "%~dp0TypeDocs.sln" %*
 
-powershell .\build.ps1 %BINDIR%
+powershell -ExecutionPolicy Unrestricted .\build.ps1 %BINDIR%
 
 GOTO EXIT
 
