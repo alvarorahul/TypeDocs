c08ed25030cc44e9c50af13dd93e8ce33606b809
Alvaro Dias <alvarorahul@hotmail.com>
2016-09-24 02:44:34 -0700

    test coverage for function documentation

---
+++ test/functions.ts
@@ -0,0 +1,39 @@
+import * as assert from "assert";
+
+import * as syntax from "../src/syntax";
+import * as typedocs from "../src/typedocs";
+
+"use strict";
+
+function getFilePath(fileName: string) {
+    return `${__dirname}\\..\\..\\test\\testcases\\${fileName}.d.ts`;
+}
+
+describe("Function", function () {
+    const elements = typedocs.generate([getFilePath("testfunctions")]);
+    it("should generate documentation elements correctly", function () {
+        assert.equal(elements.length, 1, "one function present in result");
+    });
+
+    const testFunction = <syntax.FunctionDeclaration>elements[0];
+    it("should generate documentation for test function correctly", function () {
+        assert.equal(testFunction.name, "myFunction", "name of the function is correct");
+        assert.equal(testFunction.documentation, "Documentation for test function.", "documentation of function is correct");
+    });
+
+    const param1 = testFunction.parameters[0];
+    it("should generate documentation for first parameter correctly", function () {
+        assert.equal(param1.name, "param1", "name of the parameter is correct");
+        assert.equal(param1.type, "string", "type of the parameter is correct");
+        assert.equal(param1.documentation, "The first parameter.", "documentation of parameter is correct");
+        assert.ok(!param1.optional, "param1 is not optional");
+    });
+
+    const param2 = testFunction.parameters[1];
+    it("should generate documentation for second parameter correctly", function () {
+        assert.equal(param2.name, "param2", "name of the parameter is correct");
+        assert.equal(param2.type, "number", "type of the parameter is correct");
+        assert.equal(param2.documentation, "The second parameter.", "documentation of parameter is correct");
+        assert.ok(param2.optional, "param1 is optional");
+    });
+});
---
+++ test/testcases/testfunctions.d.ts
@@ -0,0 +1,7 @@
+/**
+ * Documentation for test function.
+ * 
+ * @param param1 The first parameter.
+ * @param param2 The second parameter.
+ */
+declare function myFunction(param1: string, param2?: number): void;
